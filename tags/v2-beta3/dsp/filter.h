141931ea2e9f1f6e6bd4a504e11bb6d84b770097
sapista <sapista@8f919145-41b5-4f04-b112-5f015527c22b>
2013-04-13 16:51:04 +0000

    Tagging the v2-beta3 of eq10q

---
+++ tags/v2-beta3/dsp/filter.h
@@ -0,0 +1,273 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Pere Ràfols Soler                               *
+ *   sapista2@gmail.com                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/***************************************************************************
+This file contains the filter definitions
+****************************************************************************/
+
+#ifndef  FILTER_H
+  #define FILTER_H
+
+#include <math.h>
+
+//Constants definitions
+#define PI 3.1416
+#define  F_NOT_SET 0
+#define  F_LPF_ORDER_1 1
+#define  F_LPF_ORDER_2 2
+#define  F_LPF_ORDER_3 3
+#define  F_LPF_ORDER_4 4
+#define  F_HPF_ORDER_1 5
+#define  F_HPF_ORDER_2 6
+#define  F_HPF_ORDER_3 7
+#define  F_HPF_ORDER_4 8
+#define  F_LOW_SHELF 9
+#define  F_HIGH_SHELF 10
+#define  F_PEAK 11
+#define  F_NOTCH 12
+
+
+typedef struct
+{
+  float b0, b1, b2, a1, a2; //Second Order coeficients
+  float b1_0, b1_1, b1_2, a1_1, a1_2; //Second Order extra coeficients 
+  int filter_order;  //filter order
+  float fs; //sample rate
+  float gain, freq, q, enable;
+}Filter;
+
+typedef struct
+{
+  float buf_0;
+  float buf_1;
+  float buf_2;
+  float buf_e0;
+  float buf_e1;
+  float buf_e2; 
+}Buffers;
+
+//Initialize filter instance
+Filter *FilterInit(double rate);
+
+//Destroy a filter instance
+void FilterClean(Filter *f);
+
+//Clean buffers
+void flushBuffers(Buffers *buf);
+
+//Compute filter coeficients
+#define LOWGAIN_TO_ZERO(x) if (fabs(x) < (0.05f)) x = 0.f;
+static inline void calcCoefs(Filter *filter, float fGain, float fFreq, float fQ, int iType, float iEnabled) //p2 = GAIN p3 = Q
+{   
+    float Gain = fGain;
+    //LOWGAIN_TO_ZERO(Gain);
+    float w0=2*PI*(fFreq/filter->fs);
+    float alpha, A, b0, b1, b2, a0, a1, a2, b1_0, b1_1, b1_2, a1_0, a1_1, a1_2;
+    alpha = A = b0 = b1 = b2 = a0 = a1 = a2 = b1_0 = b1_1 = b1_2 = a1_0 = a1_1 = a1_2 = 1.0;
+    filter->filter_order = 0;
+    filter->gain = Gain;
+    filter->freq = fFreq;
+    filter->q = fQ;
+    filter->enable = iEnabled;
+    
+    switch(iType){
+
+      case F_HPF_ORDER_1:
+	b0 = 2; //b0
+	b1 = -2; //b1
+	b2 = 0.0; //b2
+	a0 = w0+2; //a0
+	a1 = w0-2; //a1
+	a2 = 0.0; //a2
+      break;
+
+      case F_HPF_ORDER_4:
+	filter->filter_order = 1;
+      case F_HPF_ORDER_2: 
+	alpha = sinf(w0)/(2*fQ);
+	b1_0 = b0 = (1 + cosf(w0))/2; //b0
+	b1_1 = b1 = -(1 + cosf(w0)); //b1
+	b1_2 = b2 = (1 + cosf(w0))/2; //b2
+	a1_0 = a0 = 1 + alpha; //a0
+	a1_1 = a1 = -2*cosf(w0); //a1
+	a1_2 = a2 = 1 - alpha; //a2
+      break;
+
+      case F_HPF_ORDER_3:
+	filter->filter_order = 1;
+	alpha = sinf(w0)/(2*fQ);
+	b1_0 = 2; //b0
+	b1_1 = -2; //b1
+	b1_2 = 0.0; //b2
+	a1_0 = w0+2; //a0
+	a1_1 = w0-2; //a1
+	a1_2 = 0.0; //a2
+	b0 = (1 + cosf(w0))/2; //b0
+	b1 = -(1 + cosf(w0)); //b1
+	b2 = (1 + cosf(w0))/2; //b2
+	a0 = 1 + alpha; //a0
+	a1 = -2*cosf(w0); //a1
+	a2 = 1 - alpha; //a2
+      break;
+
+      case F_LPF_ORDER_1: 
+	b0 = w0; //b0
+	b1 = w0; //b1
+	b2 = 0.0; //b2
+	a0 = w0+2; //a0
+	a1 = w0-2; //a1
+	a2 = 0.0; //a2
+      break;
+ 
+      case F_LPF_ORDER_4:
+	filter->filter_order = 1;
+      case F_LPF_ORDER_2:
+	alpha = sinf(w0)/(2*fQ);
+	b1_0 = b0 = (1 - cosf(w0))/2; //b0
+	b1_1 = b1 = 1 - cosf(w0); //b1
+	b1_2 = b2 = (1 - cosf(w0))/2; //b2
+	a1_0 = a0 = 1 + alpha; //a0
+	a1_1 = a1 = -2*cosf(w0); //a1
+	a1_2 = a2 = 1 - alpha; //a2
+      break;
+
+      case F_LPF_ORDER_3:
+	filter->filter_order = 1;
+	alpha = sinf(w0)/(2*fQ);
+	b1_0 = w0; //b0
+	b1_1 = w0; //b1
+	b1_2 = 0.0; //b2
+	a1_0 = w0+2; //a0
+	a1_1 = w0-2; //a1
+	a1_2 = 0.0; //a2
+	b0 = (1 - cosf(w0))/2; //b0
+	b1 = 1 - cosf(w0); //b1
+	b2 = (1 - cosf(w0))/2; //b2
+	a0 = 1 + alpha; //a0
+	a1 = -2*cosf(w0); //a1
+	a2 = 1 - alpha; //a2
+      break;
+
+      case F_LOW_SHELF:
+	A = sqrtf((Gain));
+	alpha =sinf(w0)/2 * (1/fQ);
+	b0 = A*((A+1)-(A-1)*cosf(w0)+2*sqrtf(A)*alpha); //b0
+	b1 = 2*A*((A-1)-(A+1)*cosf(w0)); //b1
+	b2 = A*((A+1)-(A-1)*cosf(w0)-2*sqrtf(A)*alpha); //b2
+	a0 = (A+1) + (A-1)*cosf(w0) + 2*sqrtf(A)*alpha; //a0
+	a1 = -2*((A-1) + (A+1)*cosf(w0)); //a1
+	a2 = (A+1) + (A-1)*cosf(w0) - 2*sqrtf(A)*alpha; //a2
+      break;
+
+      case F_HIGH_SHELF:
+	A = sqrtf((Gain));
+	alpha =sinf(w0)/2 * (1/fQ);
+	b0 = A*( (A+1) + (A-1)*cosf(w0) + 2*sqrtf(A)*alpha ); //b0
+	b1 = -2*A*( (A-1) + (A+1)*cosf(w0)); //b1
+	b2 = A*( (A+1) + (A-1)*cosf(w0) - 2*sqrtf(A)*alpha ); //b2
+	a0 = (A+1) - (A-1)*cosf(w0) + 2*sqrtf(A)*alpha; //a0
+	a1 = 2*( (A-1) - (A+1)*cosf(w0)); //a1
+	a2 = (A+1) - (A-1)*cosf(w0) - 2*sqrtf(A)*alpha; //a2
+      break;
+
+      case F_PEAK:
+	A = sqrtf((Gain));
+	//A=powf(10,(Gain/40));
+	alpha = sinf(w0)/(2*fQ);
+
+	b0 = 1 + alpha*A; //b0
+	b1 =  -2*cosf(w0); //b1
+	b2 =   1 - alpha*A; //b2
+	a0 =   1 + alpha/A; //a0
+	a1 =  -2*cosf(w0); //a1
+	a2 =   1 - alpha/A; //a2
+      break;
+
+      case F_NOTCH:
+	alpha = sinf(w0)/(2*fQ);
+
+	b0 =  1; //b0
+	b1 = -2*cosf(w0); //b1
+	b2 =  1; //b2
+	a0 =  1 + alpha; //a0
+	a1 = -2*cosf(w0); //a1
+	a2 =  1 - alpha; //a2
+      break;
+  } //End of switch
+
+    //Normalice coeficients to a0=1 and apply iEnabled
+    filter->b0 = (b0/a0)* iEnabled + (1.0f - iEnabled); //b0
+    filter->b1 = (b1/a0)* iEnabled; //b1
+    filter->b2 = (b2/a0)* iEnabled; //b2
+    filter->a1 = (a1/a0)* iEnabled; //a1
+    filter->a2 = (a2/a0)* iEnabled; //a2
+    filter->b1_0 = (b1_0/a1_0)* iEnabled + (1.0f - iEnabled);
+    filter->b1_1 = (b1_1/a1_0)* iEnabled;
+    filter->b1_2 = (b1_2/a1_0)* iEnabled;
+    filter->a1_1 = (a1_1/a1_0)* iEnabled;
+    filter->a1_2 = (a1_2/a1_0)* iEnabled;
+}
+
+
+/***************************************************************************
+///TODO I ara com faig aixo pq els HPF no fagis soroll en variar freq????
+static inline void adjustBuffers(Filter *filter, Buffers *buf)
+{
+  //Re-calc buffer values only for LPF Filters this is a improved smooth method that only works for LPF
+  if( (int)iType == LPF_ORDER_2 || (int)iType == LPF_ORDER_4)
+  {
+      //Using B
+      buf->buf_1 *= (filter->b1 / b1);
+      buf->buf_2 *= (filter->b2 / b2);
+      buf->buf_e1 *= (filter->b1_1 / b1_1);
+      buf->buf_e2 *= (filter->b1_2 / b1_2);
+  }
+}
+*********************************************************************************/
+
+#define DENORMAL_TO_ZERO(x) if (fabs(x) < (10e-40)) x = 0.f;
+
+//Compute filter
+static inline  void computeFilter(Filter *filter, Buffers *buf, float *inputSample)
+{
+  //Process 1, 2 orders
+  //w(n)=x(n)-a1*w(n-1)-a2*w(n-2)
+  buf->buf_0 = *inputSample-filter->a1*buf->buf_1-filter->a2*buf->buf_2;
+  //y(n)=bo*w(n)+b1*w(n-1)+b2*w(n-2)
+  DENORMAL_TO_ZERO(buf->buf_0);
+  *inputSample = filter->b0*buf->buf_0 + filter->b1*buf->buf_1+ filter->b2*buf->buf_2;
+
+  buf->buf_2 = buf->buf_1;
+  buf->buf_1 = buf->buf_0;
+  
+  //Process 3,4 orders if apply
+  if(filter->filter_order)
+  {
+      //w(n)=x(n)-a1*w(n-1)-a2*w(n-2)
+      buf->buf_e0 = *inputSample-filter->a1_1*buf->buf_e1-filter->a1_2*buf->buf_e2;
+      //y(n)=bo*w(n)+b1*w(n-1)+b2*w(n-2)
+      DENORMAL_TO_ZERO(buf->buf_e0);
+      *inputSample = filter->b1_0*buf->buf_e0 + filter->b1_1*buf->buf_e1+ filter->b1_2*buf->buf_e2;
+
+      buf->buf_e2 = buf->buf_e1;
+      buf->buf_e1 = buf->buf_e0;
+  }
+}
+#endif
---
+++ tags/v2-beta3/gui/widgets/eqwindow.h
@@ -0,0 +1,267 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Pere Ràfols Soler                               *
+ *   sapista2@gmail.com                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef EQ_MAIN_WIN_H
+  #define EQ_MAIN_WIN_H
+
+#include <iostream>
+#include <string>
+
+#include <gtkmm/eventbox.h>
+#include <gtkmm/alignment.h>
+#include <gtkmm/box.h>
+#include <gtkmm/togglebutton.h>
+#include <gtkmm/alignment.h>
+#include <gtkmm/button.h>
+#include <gtkmm/messagedialog.h>
+#include <gtkmm/frame.h>
+#include <gtkmm/image.h>
+
+#include <cmath>
+
+//LV2 UI header
+#include "../lv2_ui.h"
+
+#include "bandctl.h"
+#include "gainctl.h"
+#include "eqparams.h"
+#include "bodeplot.h"
+
+//Include eq definition
+#include "../eq_defines.h"
+
+#define IMAGE_LOGO_PATH "icons/logoeq10q.png"
+#define TIMER_VALUE_MS 100
+
+//Test print information, comment out for the final release
+//#define PRINT_DEBUG_INFO
+
+using namespace sigc;
+
+class EqMainWindow : public Gtk::EventBox
+{
+  public:
+    EqMainWindow(int iAudioChannels, int iNumBands, const char *uri, const char *bundlePath);
+    virtual ~EqMainWindow();   
+    
+    // Informing GUI about changes in the control ports
+    void gui_port_event(LV2UI_Handle ui, uint32_t port, uint32_t buffer_size, uint32_t format, const void * buffer)
+    {
+      float data = * static_cast<const float*>(buffer);
+      
+      
+      #ifdef PRINT_DEBUG_INFO
+	std::cout<<"gui_port_event Entring....... "<<std::endl;
+      #endif
+      
+        // Checking if params are the same as specified in the LV2 documentation
+        if (format != 0) {
+	    #ifdef PRINT_DEBUG_INFO
+	      std::cout<<"\t-- Return Format != 0"<<std::endl;
+	    #endif
+            return;
+        }
+        if (buffer_size != 4) {
+	    #ifdef PRINT_DEBUG_INFO
+	      std::cout<<"\t-- Return buffer_size != 4"<<std::endl;
+	    #endif  
+            return;
+        }
+
+        // Updating values in GUI ========================================================
+	switch (port)
+	{
+	  case EQ_BYPASS:
+	    m_bypassValue = data > 0.5 ? 1 : 0;
+	    m_port_event_Bypass = true; //Marck port even boolean
+	    ///m_BypassButton.set_active(data > 0.5);
+	    #ifdef PRINT_DEBUG_INFO
+	      std::cout<<"\t-- BYPASS"<<std::endl;
+	    #endif  
+	  break;
+
+	  case EQ_INGAIN:
+	    //m_InGainValue = data;
+	    m_CurParams->setInputGain(data);
+	    m_port_event_InGain = true; //Marck port even boolean
+	    ///m_InGain->setGain(data);
+	    
+	    #ifdef PRINT_DEBUG_INFO
+	      std::cout<<"\t-- Input Gain"<<std::endl;
+	    #endif  
+	  break;
+
+	  case EQ_OUTGAIN:
+	    //m_OutGainValue = data;
+	    m_CurParams->setOutputGain(data);
+	    m_port_event_OutGain = true; //Marck port even boolean
+	    ///m_OutGain->setGain(data);
+	    #ifdef PRINT_DEBUG_INFO
+	      std::cout<<"\t-- Out Gain"<<std::endl;
+	    #endif  
+	  break;
+
+	  default:
+	    //Connect BandGain ports
+	    if((int)port >= (PORT_OFFSET + 2*m_iNumOfChannels) && (int)port < (PORT_OFFSET + 2*m_iNumOfChannels + m_iNumOfBands))
+	    {
+	      ///m_BandCtlArray[(int)port - PORT_OFFSET - 2*m_iNumOfChannels]->setGain(data);
+	      ///m_Bode->setBandGain((int)port - PORT_OFFSET - 2*m_iNumOfChannels, data);
+	      m_CurParams->setBandGain((int)port - PORT_OFFSET - 2*m_iNumOfChannels, data);
+	      m_port_event_Curve = true; //Marck port even boolean
+	      m_port_event_Curve_Gain[(int)port - PORT_OFFSET - 2*m_iNumOfChannels] = true;
+	      #ifdef PRINT_DEBUG_INFO
+		std::cout<<"\t-- Band Gain"<<std::endl;
+	      #endif  
+	    }
+
+	    //Connect BandFreq ports
+	    else if((int)port >= (PORT_OFFSET + 2*m_iNumOfChannels + m_iNumOfBands) && (int)port < (PORT_OFFSET + 2*m_iNumOfChannels + 2*m_iNumOfBands))
+	    {
+	      ///m_BandCtlArray[(int)port - PORT_OFFSET - 2*m_iNumOfChannels - m_iNumOfBands]->setFreq(data);
+	      ///m_Bode->setBandFreq((int)port - PORT_OFFSET - 2*m_iNumOfChannels - m_iNumOfBands, data);
+	      m_CurParams->setBandFreq((int)port - PORT_OFFSET - 2*m_iNumOfChannels - m_iNumOfBands, data);
+	      m_port_event_Curve = true; //Marck port even boolean
+	      m_port_event_Curve_Freq[(int)port - PORT_OFFSET - 2*m_iNumOfChannels - m_iNumOfBands] = true;
+	      #ifdef PRINT_DEBUG_INFO
+		std::cout<<"\t-- Band Freq"<<std::endl;
+	      #endif  
+	    }
+
+	    //Connect BandParam ports
+	    else if((int)port >= (PORT_OFFSET + 2*m_iNumOfChannels + 2*m_iNumOfBands) && (int)port < (PORT_OFFSET + 2*m_iNumOfChannels + 3*m_iNumOfBands))
+	    {
+	      ///m_BandCtlArray[(int)port - PORT_OFFSET - 2*m_iNumOfChannels - 2*m_iNumOfBands]->setQ(data);
+	      ///m_Bode->setBandQ((int)port - PORT_OFFSET - 2*m_iNumOfChannels - 2*m_iNumOfBands, data);
+	      m_CurParams->setBandQ((int)port - PORT_OFFSET - 2*m_iNumOfChannels - 2*m_iNumOfBands, data);
+	      m_port_event_Curve = true; //Marck port even boolean
+	      m_port_event_Curve_Q[(int)port - PORT_OFFSET - 2*m_iNumOfChannels - 2*m_iNumOfBands] = true;
+	      #ifdef PRINT_DEBUG_INFO
+		std::cout<<"\t-- Band Q"<<std::endl;
+	      #endif  
+	    }
+
+	    //Connect BandType ports
+	    else if((int)port >= (PORT_OFFSET + 2*m_iNumOfChannels + 3*m_iNumOfBands) && (int)port < (PORT_OFFSET + 2*m_iNumOfChannels + 4*m_iNumOfBands))
+	    {
+	      ///m_BandCtlArray[(int)port - PORT_OFFSET - 2*m_iNumOfChannels - 3*m_iNumOfBands]->setFilterType(data);
+	      ///m_Bode->setBandType((int)port - PORT_OFFSET - 2*m_iNumOfChannels - 3*m_iNumOfBands, data);
+	      m_CurParams->setBandType((int)port - PORT_OFFSET - 2*m_iNumOfChannels - 3*m_iNumOfBands, (int)data);
+	      m_port_event_Curve = true; //Marck port even boolean
+	      m_port_event_Curve_Type[(int)port - PORT_OFFSET - 2*m_iNumOfChannels - 3*m_iNumOfBands] = true;
+	      #ifdef PRINT_DEBUG_INFO
+		std::cout<<"\t-- Band Type"<<std::endl;
+	      #endif  
+	    }
+
+	    //Connect BandEnabled ports
+	    else if((int)port >= (PORT_OFFSET + 2*m_iNumOfChannels + 4*m_iNumOfBands) && (int)port < (PORT_OFFSET + 2*m_iNumOfChannels + 5*m_iNumOfBands))
+	    {
+	      ///m_BandCtlArray[(int)port - PORT_OFFSET - 2*m_iNumOfChannels - 4*m_iNumOfBands]->setEnabled(data > 0.5);
+	      ///m_Bode->setBandEnable((int)port - PORT_OFFSET - 2*m_iNumOfChannels - 4*m_iNumOfBands, data > 0.5);
+	      m_CurParams->setBandEnabled((int)port - PORT_OFFSET - 2*m_iNumOfChannels - 4*m_iNumOfBands, data > 0.5);
+	      m_port_event_Curve = true; //Marck port even boolean
+	      m_port_event_Curve_Enable[(int)port - PORT_OFFSET - 2*m_iNumOfChannels - 4*m_iNumOfBands] = true;
+	      #ifdef PRINT_DEBUG_INFO
+		std::cout<<"\t-- Band Enabled"<<std::endl;
+	      #endif  
+	    }
+
+	    //Connect VuInput ports
+	    else if((int)port >= (PORT_OFFSET + 2*m_iNumOfChannels + 5*m_iNumOfBands) && (int)port < (PORT_OFFSET + 2*m_iNumOfChannels + 5*m_iNumOfBands + m_iNumOfChannels))
+	    {
+	      m_InGain->setVu((int)port - PORT_OFFSET - 2*m_iNumOfChannels - 5*m_iNumOfBands,data);
+	      #ifdef PRINT_DEBUG_INFO
+		std::cout<<"\t-- Vu input"<<std::endl;
+	      #endif  
+	    }
+
+	    //Connect VuOutput ports
+	    else if((int)port >= (PORT_OFFSET + 2*m_iNumOfChannels + 5*m_iNumOfBands + m_iNumOfChannels) && (int)port < (PORT_OFFSET + 2*m_iNumOfChannels + 5*m_iNumOfBands + 2*m_iNumOfChannels))
+	    {
+	      m_OutGain->setVu((int)port - PORT_OFFSET - 2*m_iNumOfChannels - 5*m_iNumOfBands - m_iNumOfChannels, data);
+	      #ifdef PRINT_DEBUG_INFO
+		std::cout<<"\t-- Vu output"<<std::endl;
+	      #endif  
+	    }
+	    
+	    //
+	    else
+	    {
+	      #ifdef PRINT_DEBUG_INFO	    
+		std::cout<<"\t--  Return port index is out of range"<<std::endl;
+	      #endif
+	      return;
+	    }
+	  break;
+	}       
+        
+	#ifdef PRINT_DEBUG_INFO	    
+	  std::cout<<"\t--  Return OK"<<std::endl;
+	#endif
+	
+    }
+
+    LV2UI_Controller controller;
+    LV2UI_Write_Function write_function;
+
+  protected:
+    EqParams *m_AParams, *m_BParams, *m_CurParams;
+    BandCtl **m_BandCtlArray; 
+    GainCtl *m_InGain, *m_OutGain;
+    Gtk::HBox m_BandBox, m_ABFlatBox, m_GainEqBox;
+    Gtk::VBox m_CurveBypassBandsBox, m_MainBox;
+    Gtk::ToggleButton m_BypassButton, m_AButton, m_BButton;
+    Gtk::Alignment m_FlatAlign, m_ABAlign, m_ButtonAAlign, m_ButtonBAlign, m_BypassAlign, m_LoadAlign, m_SaveAlign;
+    Gtk::Button m_FlatButton, m_SaveButton, m_LoadButton;
+    Gtk::Frame m_PlotFrame;
+    Gtk::Alignment m_MainWidgetAlign;
+    PlotEQCurve *m_Bode;
+    Gtk::Image *image_logo_center;
+
+    void loadEqParams();
+    void changeAB(EqParams *toBeCurrent);
+    void saveToFile();
+    void loadFromFile();
+    
+    //Signal Handlers
+    void onButtonA();
+    void onButtonB();
+    void onButtonFlat();
+    void onButtonBypass();
+    void onBandChange(int iBand, int iField, float fValue);
+    void onInputGainChange();
+    void onOutputGainChange();
+    void onRealize();
+    void onCurveChange(int band_ix, float Gain, float Freq, float Q);
+    void onCurveBandEnable(int band_ix, bool IsEnabled);
+    bool on_timeout();
+    
+  private:
+    float m_bypassValue;   
+    const int m_iNumOfChannels;
+    const int m_iNumOfBands;
+    bool m_bMutex, m_port_event_InGain, m_port_event_OutGain, m_port_event_Bypass, m_port_event_Curve;
+    bool *m_port_event_Curve_Gain, *m_port_event_Curve_Freq, *m_port_event_Curve_Q, *m_port_event_Curve_Type, *m_port_event_Curve_Enable;
+    std::string m_pluginUri;
+    std::string m_bundlePath;    
+};
+
+#endif
